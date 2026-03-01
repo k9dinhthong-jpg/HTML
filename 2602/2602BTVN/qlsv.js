9c4f35466210b9b9f8280000e7c215538d3147f4
Sugar <k9dinhthong@gmail.com>
2026-03-01 14:58:02 +0700

    update all files

--- 2602/2602BTVN/qlsv.js
+++ 2602/2602BTVN/qlsv.js
@@ -1,3 +1,3 @@
-if (localStorage.getItem("login") !== "true") {
-  window.location.href = "login.html";
-}
+// if (localStorage.getItem("login") !== "true") {
+//   window.location.href = "login.html";
+// }
