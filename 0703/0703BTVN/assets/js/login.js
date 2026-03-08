e1e22f1ceeb50c8ad250f87ecc63d575bea218ef
Sugar <k9dinhthong@gmail.com>
2026-03-09 00:00:56 +0700

    update all files

---
+++ 0703/0703BTVN/assets/js/login.js
@@ -0,0 +1,13 @@
+/* Đăng nhập */
+function login(e) {
+  e.preventDefault();
+  let user = document.querySelector("#username").value;
+  let pass = document.querySelector("#password").value;
+  if (user === "admin" && pass === "123456") {
+    window.location.href = "qlsv.html";
+  } else {
+    alert("Sai tài khoản hoặc mật khẩu!");
+  }
+}
+let clickLogin = document.querySelector(".click-Login");
+clickLogin.addEventListener("click", login);
