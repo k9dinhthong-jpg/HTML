c21a70433c63d266a042d8a18a25661d0bc85377
Sugar <k9dinhthong@gmail.com>
2026-03-09 15:14:47 +0700

    update all files

---
+++ 0301/B2/assets/js/script.js
@@ -0,0 +1,5 @@
+let clickGuilienhe = document.querySelector("#guilienhe");
+clickGuilienhe.addEventListener("click  ", function (event) {
+  event.preventDefault();
+  console.log("Gửi liên hệ đã được nhấn!");
+});
