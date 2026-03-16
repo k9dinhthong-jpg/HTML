0d30c954f8279bbf7c0c661a6e4cb9ce984d12d2
Sugar <k9dinhthong@gmail.com>
2026-03-16 21:07:15 +0700

    update all files

---
+++ Sugar/assets/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    nav: true,
+    dots: true,
+    autoplayHoverPause: true,
+    autoplaySpeed: 1500,
+  });
+});
