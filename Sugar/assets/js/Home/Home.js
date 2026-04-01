6b30ce485db9149c514969b06c4fb94a0a5a1089
Sugar <k9dinhthong@gmail.com>
2026-04-01 16:39:34 +0700

    update all files

---
+++ Sugar/assets/js/Home/Home.js
@@ -0,0 +1,40 @@
+// Dùng để chuyển slider ảnh trên trang chủ
+$(document).ready(function () {
+  $(".owl-carousel").owlCarousel({
+    items: 1,
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 4000,
+    nav: true,
+    dots: true,
+    autoplayHoverPause: true,
+    autoplaySpeed: 1500,
+  });
+});
+
+const sitenav = document.querySelector(".site-nav");
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > window.innerHeight * 0.02) {
+    sitenav.classList.add("sticky-top");
+  } else {
+    sitenav.classList.remove("sticky-top");
+  }
+});
+// Làm sáng thẻ khi trang đang ở thẻ đó
+const navLinks = document.querySelectorAll(".site-nav .nav-link");
+
+navLinks.forEach((link) => {
+  link.addEventListener("click", function () {
+    navLinks.forEach((nav) => nav.classList.remove("active"));
+    this.classList.add("active");
+  });
+});
+// Hiển thị menu khi click vào biểu tượng
+document.addEventListener("DOMContentLoaded", function () {
+  const menuToggle = document.getElementById("menu-Toggle");
+  const navMenu = document.getElementById("nav-Menu");
+  menuToggle.addEventListener("click", function () {
+    navMenu.classList.toggle("show");
+  });
+});
